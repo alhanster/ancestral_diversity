01cf17e5221de100fe347f634c9d4be051dd279c
alhanster <59197058+alhanster@users.noreply.github.com>
2024-05-07 15:09:47 -0500

    Add files via upload

---
+++ scripts/GeneList_VariantAnnotations.R
@@ -0,0 +1,25 @@
+################################################################################
+# Purpose: Pulling Gene Lists and Variant Annotations                          #
+# Author: Alexander Han                                                        #
+################################################################################
+
+# Variant Annotations
+SYN <- c("synonymous_variant")
+MISSENSE <- c("missense_variant", "inframe_deletion", "inframe_insertion")
+LOF <- c("frameshift_variant", "stop_gained", "start_lost", 
+         "splice_acceptor_variant", "splice_donor_variant",
+         "stop_gained&frameshift_variant", "splice_donor_variant&coding_sequence_variant&intron_variant", 
+         "splice_donor_variant&intron_variant", "stop_gained&inframe_insertion", "frameshift_variant&stop_lost", 
+         "frameshift_variant&start_lost", "start_lost&splice_region_variant", 
+         "stop_gained&protein_altering_variant", "stop_gained&frameshift_variant&splice_region_variant", 
+         "stop_gained&inframe_deletion", "frameshift_variant&stop_retained_variant")
+
+# Gene Lists
+setwd("~/Library/CloudStorage/Box-Box/Dhindsa Lab/RVIS")
+dee_monoallelic <- fread("Genes/dee_monoallelic.tsv", header = F)
+asd_monoallelic <- fread("Genes/asd_monoallelic.tsv", header = F)
+dd_monoallelic <- fread("Genes/dd_monoallelic.tsv", header = F)
+
+setwd("~/Library/CloudStorage/Box-Box/RVIS/data/gene_lists")
+clingen_HI <- fread("clingen_HI.txt", header = F)
+mgi_essential <- fread("mgi_essential.tsv", header = F)
