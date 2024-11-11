df2ea2cfe24391fda2c301e64890a32cb3753a7b
Alexander Han <59197058+alhanster@users.noreply.github.com>
2024-11-11 09:58:15 -0600

    Update Figure4_LOF_OE.R

--- figures/Figure4_LOF_OE.R
+++ figures/Figure4_LOF_OE.R
@@ -79,7 +79,7 @@ mgi_delong <- mgi_results$delong_results %>%
 
 HI_results <- PrintAUC(LOF_OE, score_cols, clingen_HI)
 HI_AUC <- HI_results$auc_df %>% 
-  rename(`Haploinsufficient\n (n=360)` = AUC) %>% 
+  rename(`Haploinsufficient\n (n=390)` = AUC) %>% 
   pivot_longer(cols = -1, names_to = "Gene List", values_to = "AUC")
 HI_delong <- HI_results$delong_results %>% 
   mutate(Gene_List = "Haploinsufficient") %>% 
@@ -127,7 +127,7 @@ write.csv(UKB_LOF_OE_log, "output/LOF_OE_LogRegression.csv", row.names = FALSE)
 a <- PrintGraph(LOF_OE_AUC, "DEE Monoallelic\n (n=94)") + theme(legend.position = "none") 
 b <- PrintGraph(LOF_OE_AUC, "DD Monoallelic\n (n=435)") + theme(legend.position = "none") + labs(y = "")
 c <- PrintGraph(LOF_OE_AUC, "ASD Monoallelic\n (n=190)") + theme(legend.position = "none") + labs(y = "")
-d <- PrintGraph(LOF_OE_AUC, "Haploinsufficient\n (n=360)") + theme(legend.position = "none") + labs(y = "")
+d <- PrintGraph(LOF_OE_AUC, "Haploinsufficient\n (n=390)") + theme(legend.position = "none") + labs(y = "")
 e <- PrintGraph(LOF_OE_AUC, "Mouse Essential\n (n=2454)") + theme(legend.position = "none") + labs(y = "")
 
 library(patchwork)
